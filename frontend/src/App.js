b74beaf27bc76634f206bf24c07cf60c6c3c0869
kelley <kelleyc2@illinois.edu>
2019-01-27 12:52:25 -0600

    Added input text and button with functions (part 5)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,31 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      inputValue: '',
       shows: [
         {id: 1, name: "Game of Thrones", episodes_seen: 0},
         {id: 2, name: "Naruto", episodes_seen: 220},
         {id: 3, name: "Black Mirror", episodes_seen: 3},
       ]
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmitClick = this.handleSubmitClick.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmitClick() {
+    this.setState({
+      inputValue: '',
+      shows: this.state.shows.concat({
+        id: this.state.shows[this.state.shows.length - 1].id + 1,
+        name: this.state.inputValue,
+        episodes_seen: 0
+      })
+    })
   }
 
   render() {
@@ -21,6 +40,13 @@ class App extends Component {
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
+
+        <div>
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+          <button type="button" onClick={this.handleSubmitClick}>
+            Submit
+          </button>
+        </div>
       </div>
     )
   }
