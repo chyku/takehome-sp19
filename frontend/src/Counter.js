d1ee41972fd1c292f5b756a69573f087402b4d26
kelley <kelleyc2@illinois.edu>
2019-01-27 10:48:50 -0600

    Added increment/decrement buttons and onClick functions

--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -6,11 +6,25 @@ class Counter extends Component {
   	super(props)
   	this.state = {count: 0}
   }
+
+  handleIncrementClick() {
+  	this.setState({count: this.state.count + 1})
+  }
+
+  handleDecrementClick() {
+  	this.setState({count: this.state.count - 1})
+  }
   
   render() {
     return (
       <div>
       	{this.state.count}
+      	<button onClick={this.handleIncrementClick}>
+      		+
+      	</button>
+      	<button onClick={this.handleDecrementClick}>
+      		-
+      	</button>
       </div>
     )
   }
