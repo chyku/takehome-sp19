d4ffe1510df4900ff61e47e060a24961dfb63994
kelley <kelleyc2@illinois.edu>
2019-01-27 11:52:05 -0600

    Added checking for negative count

--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -14,7 +14,9 @@ class Counter extends Component {
   }
 
   handleDecrementClick() {
-  	this.setState({count: this.state.count - 1})
+  	if (this.state.count > 0) {
+  		this.setState({count: this.state.count - 1})
+  	}
   }
   
   render() {
