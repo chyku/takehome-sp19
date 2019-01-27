915334ea1baaccf6aff38c186cd63c7767df2642
kelley <kelleyc2@illinois.edu>
2019-01-27 10:22:02 -0600

    Passed complete to instructions (part 1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,9 +15,11 @@ class App extends Component {
   }
 
   render() {
+    const complete = true;
+    
     return (
       <div className="App">
-        <Instructions />
+        <Instructions complete={complete}/>
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
